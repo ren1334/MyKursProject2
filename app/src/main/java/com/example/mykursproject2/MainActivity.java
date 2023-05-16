f53597b8581db83d6051599c0dd0f4f940f0350a
ren1334 <temyr98tigr@gmail.com>
2023-05-16 23:47:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mykursproject2/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.mykursproject2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView userList;
+    DatabaseHelper databaseHelper;
+    SQLiteDatabase db;
+    Cursor userCursor;
+    SimpleCursorAdapter userAdapter;
+    private EditText nameBox1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        userList = (ListView)findViewById(R.id.list);
+        nameBox1 = (EditText)findViewById(R.id.editTextTextPersonName);
+        userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+                intent.putExtra("id", id);startActivity(intent);
+            }
+        });
+
+        databaseHelper = new DatabaseHelper(getApplicationContext());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // открываем подключение
+        db = databaseHelper.getReadableDatabase();
+
+        //получаем данные из бд в виде курсора
+        userCursor =  db.rawQuery("select * from "+ DatabaseHelper.TABLE, null);
+        // определяем, какие столбцы из курсора будут выводиться в ListView
+        String[] headers = new String[] {DatabaseHelper.COLUMN_NAME, DatabaseHelper.COLUMN_YEAR};
+        // создаем адаптер, передаем в него курсор
+        userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
+                userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        userList.setAdapter(userAdapter);
+    }
+    // по нажатию на кнопку запускаем UserActivity для добавления данных
+    public void add(View view){
+        Intent intent = new Intent(this, UserActivity.class);
+        startActivity(intent);
+    }
+    public void onClSearch(View view){
+       // nameBox1.getText().toString());
+
+        db = databaseHelper.getReadableDatabase();
+
+        //получаем данные из бд в виде курсора
+        //userCursor =  db.rawQuery("select * from "+ DatabaseHelper.TABLE, null);
+
+        userCursor =   db.rawQuery("select * from " + DatabaseHelper.TABLE + " where " +
+                DatabaseHelper.COLUMN_NAME + " like ?", new String[]{"%" + nameBox1.getText().toString() + "%"});
+
+        // определяем, какие столбцы из курсора будут выводиться в ListView
+        String[] headers = new String[] {DatabaseHelper.COLUMN_NAME, DatabaseHelper.COLUMN_YEAR};
+        // создаем адаптер, передаем в него курсор
+        userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
+                userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        userList.setAdapter(userAdapter);
+    }
+
+    public void onClNamSaerch(View view){
+        // nameBox1.getText().toString());
+
+        db = databaseHelper.getReadableDatabase();
+
+        //получаем данные из бд в виде курсора
+        //userCursor =  db.rawQuery("select * from "+ DatabaseHelper.TABLE, null);
+
+
+        userCursor =   db.rawQuery("select * from " + DatabaseHelper.TABLE + " where " +
+                DatabaseHelper.COLUMN_YEAR + " like ?", new String[]{"%" + nameBox1.getText().toString() + "%"});
+        // определяем, какие столбцы из курсора будут выводиться в ListView
+        String[] headers = new String[] {DatabaseHelper.COLUMN_NAME, DatabaseHelper.COLUMN_YEAR};
+        // создаем адаптер, передаем в него курсор
+        userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
+                userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        userList.setAdapter(userAdapter);
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        // Закрываем подключение и курсор
+        db.close();
+        userCursor.close();
+    }
+
+}
